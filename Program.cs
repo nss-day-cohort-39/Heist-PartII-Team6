c6cb7109fa3eb5bb6b0a01e38fd071489a21077d
Daniel Hero <danielhero@outlook.com>
2020-06-05 14:16:56 -0500

    added rolodex

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Heist_PartTwo
 {
@@ -6,7 +7,62 @@ namespace Heist_PartTwo
     {
         static void Main(string[] args)
         {
+            Hacker MarkDaHackerz = new Hacker()
+            {
+                Name = "Mark McCann",
+                SkillLevel = 38,
+                PercentageCut = 20
+            };
 
+            LockSpecialist TimDaToolMan = new LockSpecialist()
+            {
+                Name = "Time George",
+                SkillLevel = 25,
+                PercentageCut = 20
+            };
+
+            Muscle ThomasDaTank = new Muscle()
+            {
+                Name = "Thomas Long",
+                SkillLevel = 45,
+                PercentageCut = 20
+            };
+
+            Hacker FakeDaniel = new Hacker()
+            {
+                Name = "Daniel Fuqua",
+                SkillLevel = 50,
+                PercentageCut = 20
+            };
+
+            Muscle RealDaniel = new Muscle()
+            {
+                Name = "Daniel Hero",
+                SkillLevel = 40,
+                PercentageCut = 20
+            };
+
+            List<IRobber> Rolodex = new List<IRobber>()
+            {
+                MarkDaHackerz,
+                TimDaToolMan,
+                ThomasDaTank,
+                FakeDaniel,
+                RealDaniel
+            };
+
+            Console.WriteLine("Enter the name of new crew member.");
+            Console.ReadLine();
+
+            Console.WriteLine("Choose a specialty");
+            Console.WriteLine("Hacker | Muscle | LockpickSpecialist");
+            Console.ReadLine();
+
+            Console.WriteLine("Choose skill level between 1 and 100");
+            Console.ReadLine();
+
+            Console.WriteLine("Choose the cut percentage");
+            Console.ReadLine();
         }
     }
 }
